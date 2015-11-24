15fe8ebb7e2a9d6754ca27cb5a163342c1366ed6
Moses <rlakim@ucdavis.edu>
2015-11-24 14:54:40 -0800

    Working on basic UIdesign and functions

--- PlayWhileWait/PlayWhileWait/ViewController.swift
+++ PlayWhileWait/PlayWhileWait/ViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+
+    // MARK: Waiting Time Label Text
+    @IBOutlet weak var placeHolderForMin: UILabel!
+    @IBOutlet weak var betweenPlaceHolder: UILabel!
+    @IBOutlet weak var placeHolderForSec: UILabel!
+    
+    // MARK: App Title(Temporary)
+    @IBOutlet weak var appTitle: UILabel!
+    
+    // MARK: Clock UI
+    // FIXME: The PNG file is not high resolution
+    @IBOutlet weak var frameCircle: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBOutlet weak var appTitle: UILabel!
-
+    
+    func zPosition() {
+        frameCircle.layer.zPosition = 2
+        appTitle.layer.zPosition = 3
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
