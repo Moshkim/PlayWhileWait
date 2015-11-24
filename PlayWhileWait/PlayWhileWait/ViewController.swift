63ef7f4f56180e08e1c2b836577d3f2deb47f627
Moses <rlakim@ucdavis.edu>
2015-11-23 22:31:38 -0800

    created project for the app

---
+++ PlayWhileWait/PlayWhileWait/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayWhileWait
+//
+//  Created by KWANIL KIM on 11/23/15.
+//  Copyright © 2015 Seize.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
